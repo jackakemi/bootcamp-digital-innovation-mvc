c2d2e563cdb054c04e1749c23901b87d35017d8d
jackeline.moura <jackeline.moura@indigosoft.com.br>
2021-07-13 15:10:21 -0300

    api mvc e swegger criados

---
+++ DioBootcampMVC/DioBootcampMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DioBootcampMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
